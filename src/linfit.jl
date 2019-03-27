de992f7536055bae83d13890eb025db535aadfc4
Paulo José Saiz Jabardo <pjabardo@gmail.com>
2019-03-27 16:38:18 -0300

    Using explicit functions to compute the linear fit
    the `linear_fit` function now uses an explicit formulation for
    fitting a line. It is probably faster and more robust.

--- src/linfit.jl
+++ src/linfit.jl
@@ -1,9 +1,32 @@
 using Compat.LinearAlgebra
 
 
+sq(x) = x*x
 
 "Fits a straight line through a set of points, `y = a₁ + a₂ * x`"
-linear_fit(x, y) = hcat(fill!(similar(x),1), x) \ y
+function linear_fit(x, y)
+
+    
+    sx = sum(x)
+    sy = sum(y)
+
+    m = length(x)
+
+    sx2 = zero(sx)
+    sy2 = zero(sy)
+    sxy = zero(sx*sy)
+
+    for i = 1:m
+        sx2 += x[i]*x[i]
+        sy2 += y[i]*y[i]
+        sxy += x[i]*y[i]
+    end
+
+    a0 = (sx2*sy - sxy*sx) / ( m*sx2 - sx*sx )
+    a1 = (m*sxy - sx*sy) / (m*sx2 - sx*sx)
+
+    return [a0, a1]
+end
 
 "Fits a log function through a set of points: `y = a₁+ a₂*log(x)`"
 log_fit(x, y) = linear_fit(log.(x), y)
