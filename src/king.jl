4db3696b52d226081f1a2b84f05692836a66ba33
Paulo José Saiz Jabardo <pjabardo@gmail.com>
2020-12-02 13:21:26 -0300

    Tests passing
    I had to tweak the inner/outer constructers in king.jl to get hthings working.

--- src/king.jl
+++ src/king.jl
@@ -14,10 +14,10 @@ linear_king_fit(E, U) = linear_fit(sqrt.(U), E .* E)
 "Type that represents a Linear (original) King's law"
 struct LinearKingFit{T<:Number} <: LeastSquares
     coefs::NTuple{2,T} #Array{Float64,1}
-    LinearKingFit(coefs) where {T<:Number} = new{T}((coefs[1], coefs[2])) #copy(coefs))
-    LinearKingFit(A, B) where {T<:Number} = new{T}((A,B))
+    LinearKingFit(A::T, B::T) where {T<:Number} = new{T}((A,B))
+    LinearKingFit(coefs::NTuple{2,T}) where {T<:Number}= new{T}(coefs)
+    LinearKingFit(E::AbstractVector{T},U::AbstractVector{T}) where {T <: Number} = new{T}(linear_king_fit(E, U))
 end
-LinearKingFit(E,U) = LinearKingFit(linear_king_fit(E, U))
 
 (f::LinearKingFit)(E) = ( (E.*E .- f.coefs[1]) ./ f.coefs[2] ) .^ 2
 
@@ -40,17 +40,20 @@ function king_fit(E, U, eps=1e-8, maxiter=200)
     a0 = [f[1], f[2], 0.5]
 
     coefs, converged, niter = nonlinear_fit(hcat(E,U), kingfun, a0, eps, maxiter)
-    return coefs
+    return (coefs[1], coefs[2], coefs[3])
 end
 
 
 "Type that represents the modified King's law "
 struct KingFit{T<:Number} <: LeastSquares
     coefs::NTuple{3,T} #Array{Float64,1}
-    KingFit(coefs) where {T<:Number} = new{T}((coefs[1], coefs[2], coefs[3])) #copy(coefs))
-    KingFit(A, B, n) where {T<:Number} = new{T}((A, B, n)) #copy(coefs))
+    KingFit(A::T, B::T, n::T) where {T<:Number} = new{T}((A, B, n)) #copy(coefs))
+    KingFit(coefs::NTuple{3,T}) where {T<:Number} = new{T}(coefs)
+    KingFit(A::T, B::T) where {T<:Number} = new{T}((A, B, one(T)/2))
+    KingFit(coefs::NTuple{2,T}) where {T<:Number} = new{T}(coefs[1], coefs[2])
 end
 KingFit(E,U, eps=1e-8, maxiter=200) = KingFit(king_fit(E, U, eps, maxiter))
+
 
 (f::KingFit)(E) = ( (E*E - f.coefs[1]) / f.coefs[2]) ^ (1 / f.coefs[3])
 
